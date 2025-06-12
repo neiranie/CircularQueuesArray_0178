09b4ee9e144745807145880fda23a59ad03d5b96
neiranie <nurkhairanianneira@gmail.com>
2025-06-12 10:23:49 +0700

    checking if queue is empty

--- circularQueues.cpp
+++ circularQueues.cpp
@@ -40,8 +40,22 @@ public:
         }
         else
         {
+            // Jika REAR berada di posisi terakhir array, kembali ke awal array
+            if (REAR == max - 1)
+                REAR = 0;
+            else
+                REAR = REAR + 1;
+        }
+        queue_array[REAR] = num;
+    }
 
+    void remove()
+    {
+        // Cek apakah antrian kosong
+        if (FRONT == -1)
+        {
+            cout << "Queue underflow\n";
+            return;
         }
     }
-}
 
