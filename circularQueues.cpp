49c28d5dd7c5d6e9dad156dad55a2954fafa7c62
neiranie <nurkhairanianneira@gmail.com>
2025-06-12 10:13:38 +0700

    adding class

--- circularQueues.cpp
+++ circularQueues.cpp
@@ -1,2 +1,26 @@
 // CircularQueuesArray_0178
 // 15 commit
+
+#include <iostream>
+using namespace std;
+
+class Queues
+{
+private:
+    static const int max = 5;
+    int FRONT, REAR;
+    int queue_array[5];
+
+public:
+    Queues()
+    {
+        FRONT = -1;
+        REAR = -1;
+    }
+
+    void insert()
+    {
+        int 
+    }
+}
+
