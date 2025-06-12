707fdf48c0da32fffdc848eac1455cda13fd2343
neiranie <nurkhairanianneira@gmail.com>
2025-06-12 10:27:20 +0700

    checking if queue only have 1 element

--- circularQueues.cpp
+++ circularQueues.cpp
@@ -57,5 +57,14 @@ public:
             cout << "Queue underflow\n";
             return;
         }
+        cout << "\nThe element deleted from the queue is: " << queue_array[FRONT] << "\n";
+
+        // cek jika antrian hanya memiliki satu elemen
+        if (FRONT == REAR)
+        {
+            FRONT = -1;
+            REAR = -1;
+        }
+        else
     }
 
