9badb241e5e92608d805be86cbddf3b1644e9767
neiranie <nurkhairanianneira@gmail.com>
2025-06-12 10:31:08 +0700

    adding command if the deleted element was in the last position of array, comeback to the first array

--- circularQueues.cpp
+++ circularQueues.cpp
@@ -66,5 +66,14 @@ public:
             REAR = -1;
         }
         else
+        {
+            // Jika elemen yang dihapus berada di posisi terakhir array, kembali ke awal array
+            if (FRONT == max -1)
+                FRONT = 0;
+            else
+                FRONT = FRONT + 1;
+        }
     }
+
+    
 
