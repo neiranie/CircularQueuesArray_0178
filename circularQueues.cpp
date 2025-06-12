d458a916e24122d3d921ea1a2eb3830be053a0b8
neiranie <nurkhairanianneira@gmail.com>
2025-06-12 10:46:53 +0700

    adding command iteration from beginning until REAR

--- circularQueues.cpp
+++ circularQueues.cpp
@@ -110,7 +110,15 @@ public:
 
             FRONT_position = 0;
 
-            
+            // Iterasi dari awal hingga REAR
+            while (FRONT_position <= REAR_position)
+            {
+                cout << queue_array[FRONT_position] << "  ";
+                FRONT_position++;
+            }
+            cout << endl;
         }
+    }
+};
 
 
